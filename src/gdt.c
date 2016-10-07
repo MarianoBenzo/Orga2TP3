950dbea7ec838b770f36e52fd87d40e8ecad64c1
FerminT <FerminT@users.noreply.github.com>
2016-10-07 16:06:03 -0300

    tabla gdt con algunas dudas

--- src/gdt.c
+++ src/gdt.c
@@ -10,6 +10,7 @@
 #define kernel_data 19
 #define user_code 20
 #define user_data 21
+#define pantalla 22
 
 gdt_entry gdt[GDT_COUNT] = {
     /* Descriptor nulo*/
@@ -41,7 +42,7 @@ gdt_entry gdt[GDT_COUNT] = {
         (unsigned char)     0x07,           /* limit[16:19] */
         (unsigned char)     0x00,           /* avl          */
         (unsigned char)     0x00,           /* l            */
-        (unsigned char)     0x00,           /* db           */
+        (unsigned char)     0x01,           /* db           Acá va 1? (32-bit segment) */ 
         (unsigned char)     0x01,           /* g            */  
         (unsigned char)     0x00,           /* base[31:24]  */
     },
@@ -50,14 +51,62 @@ gdt_entry gdt[GDT_COUNT] = {
         (unsigned short)    0x0000,         /* limit[0:15]  */
         (unsigned short)    0x1200,         /* base[0:15]   */
         (unsigned char)     0x00,           /* base[23:16]  */
-        (unsigned char)     0x00,           /* type         */
-        (unsigned char)     0x00,           /* s            */
+        (unsigned char)     0x00,           /* type         Es todo read only? (todos los descriptores apuntan a la misma parte de memoria) */
+        (unsigned char)     0x00,           /* s        	Es todo system? (todos los descriptores apuntan a la misma parte de memoria)    */
+        (unsigned char)     0x00,           /* dpl          */
+        (unsigned char)     0x00,           /* p            */
+        (unsigned char)     0x07,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           Acá va 1? (32-bit segment) */ 
+        (unsigned char)     0x01,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+	
+    [user_code] = (gdt_entry) {
+        (unsigned short)    0x0000,         /* limit[0:15]  */
+        (unsigned short)    0x1200,         /* base[0:15]   */
+        (unsigned char)     0x00,           /* base[23:16]  */
+        (unsigned char)     0x00,           /* type         Es todo read only? (todos los descriptores apuntan a la misma parte de memoria) */
+        (unsigned char)     0x00,           /* s        	Es todo system? (todos los descriptores apuntan a la misma parte de memoria)    */
+        (unsigned char)     0x11,           /* dpl          */
+        (unsigned char)     0x00,           /* p            */
+        (unsigned char)     0x07,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           Acá va 1? (32-bit segment) */ 
+        (unsigned char)     0x01,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+	
+    [user_data] = (gdt_entry) {
+        (unsigned short)    0x0000,         /* limit[0:15]  */
+        (unsigned short)    0x1200,         /* base[0:15]   */
+        (unsigned char)     0x00,           /* base[23:16]  */
+        (unsigned char)     0x00,           /* type         Es todo read only? (todos los descriptores apuntan a la misma parte de memoria) */
+        (unsigned char)     0x00,           /* s        	Es todo system? (todos los descriptores apuntan a la misma parte de memoria)    */
+        (unsigned char)     0x11,           /* dpl          */
+        (unsigned char)     0x00,           /* p            */
+        (unsigned char)     0x07,           /* limit[16:19] */
+        (unsigned char)     0x00,           /* avl          */
+        (unsigned char)     0x00,           /* l            */
+        (unsigned char)     0x01,           /* db           Acá va 1? (32-bit segment) */ 
+        (unsigned char)     0x01,           /* g            */
+        (unsigned char)     0x00,           /* base[31:24]  */
+    },
+	
+    [pantalla] = (gdt_entry) {
+        (unsigned short)    0x1000,         /* limit[0:15]  1000 = 4096, en bytes*/
+        (unsigned short)    0x8000,         /* base[0:15]   */
+        (unsigned char)     0x0B,           /* base[23:16]  */
+        (unsigned char)     0x02,           /* type         Read/write? Importa el tipo si es System?*/
+        (unsigned char)     0x00,           /* s        	Es system? */
         (unsigned char)     0x00,           /* dpl          */
         (unsigned char)     0x00,           /* p            */
         (unsigned char)     0x00,           /* limit[16:19] */
         (unsigned char)     0x00,           /* avl          */
         (unsigned char)     0x00,           /* l            */
-        (unsigned char)     0x00,           /* db           */
+        (unsigned char)     0x00,           /* db           Acá va 0? (16-bit segment) */ 
         (unsigned char)     0x00,           /* g            */
         (unsigned char)     0x00,           /* base[31:24]  */
     },
